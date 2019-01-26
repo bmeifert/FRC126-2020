895ae4f0e8e3c16dcbcedb4055a43c258954cd13
Keith <kwmeifert@gmail.com>
2019-01-26 11:02:15 -0500

    Refactoring and SetRobot slider implementation

--- frc126-2019/src/main/java/org/usfirst/frc/team126/robot/Robot.java
+++ frc126-2019/src/main/java/org/usfirst/frc/team126/robot/Robot.java
@@ -1,4 +1,3 @@
-		
 package org.usfirst.frc.team126.robot;
 
 import edu.wpi.first.cameraserver.CameraServer;
@@ -6,6 +5,7 @@ import edu.wpi.first.wpilibj.Spark;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import org.usfirst.frc.team126.robot.commands.AutoCenterToLeft;
 import org.usfirst.frc.team126.robot.subsystems.Intake;
 import org.usfirst.frc.team126.robot.subsystems.InternalData;
@@ -16,10 +16,10 @@ import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
 public class Robot extends TimedRobot {
 
-	public static Command autonomous; // Create commands
+	public static Command autonomous; // Create subsystems
 	public static WestCoastDrive driveBase;
 	public static InternalData internalData;
-	public static OI oi;
+	public static BOI oi;
 	public static Intake intake;
 	public static Vision vision;
 
@@ -29,62 +29,68 @@ public class Robot extends TimedRobot {
 	public static TalonSRX right2 = new TalonSRX(RobotMap.back2);
 	public static TalonSRX left3 = new TalonSRX(RobotMap.back3);
 	public static TalonSRX right3 = new TalonSRX(RobotMap.back3);
-
-
 	public static Spark intakeMotor = new Spark(9);
-
+	public double robotID;
 
 	@SuppressWarnings("unchecked")
 	@Override
-	public void robotInit() {
-		RobotMap.setRobot(0);
-		oi = new OI();
+	public void robotInit() { // Runs when the code first starts
+		SmartDashboard.putNumber("Robot ID", 0);
+		oi = new BOI();
 		driveBase = new WestCoastDrive();
 		internalData = new InternalData();
 		intake = new Intake();
 		vision = new Vision();
 		CameraServer.getInstance().startAutomaticCapture();
+		if(Math.rint(2) == 1) {
+			System.out.println("ROBOT INIT COMPLETED - doodoo");
+		}
+		else {
+			System.out.println("ROBOT INIT COMPLETED - meatball");
+		}
 	}
 	
 	@Override
-	public void robotPeriodic() {
-
+	public void robotPeriodic() { // Runs periodically regardless of robot state
 	}
 
 	@Override
-	public void disabledInit() {
-
+	public void disabledInit() { // Runs when robot is first disabled
 	}
 
 	@Override
-	public void disabledPeriodic() {
+	public void disabledPeriodic() { // Runs periodically when robot is disabled
 		Scheduler.getInstance().run();
 	}
 	
 	@Override
-	public void autonomousInit() {
+	public void autonomousInit() { // Runs when sandstorm starts
+		robotID = SmartDashboard.getNumber("Robot ID", 0);
+		RobotMap.setRobot(robotID);
 		autonomous = (Command) new AutoCenterToLeft();
 	}
 
 	@Override
-	public void autonomousPeriodic() {
-		Scheduler.getInstance().run();
+	public void autonomousPeriodic() { // Runs periodically during sandstorm
+		Scheduler.getInstance().run(); 
 		
 	}
 
 	@Override
-	public void teleopInit() {
+	public void teleopInit() { // Runs when sandstorm ends
+		robotID = SmartDashboard.getNumber("Robot ID", 0);
+		RobotMap.setRobot(robotID);
 		if(autonomous != null){
 			autonomous.cancel();
 		}
     }
 
 	@Override
-	public void teleopPeriodic() {
+	public void teleopPeriodic() { // Runs periodically during teleop
 		Scheduler.getInstance().run();
 	}
 
 	@Override
-	public void testPeriodic() {
+	public void testPeriodic() { // Runs during test (not implemented)
 	}
 }
