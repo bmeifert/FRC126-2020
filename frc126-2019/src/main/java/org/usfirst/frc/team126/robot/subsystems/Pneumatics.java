182efe01cba09b977a532e9051a06e692f2ec1ff
Keith <kwmeifert@gmail.com>
2019-01-27 10:16:58 -0500

    Added subsystem boilerplate

---
+++ frc126-2019/src/main/java/org/usfirst/frc/team126/robot/subsystems/Pneumatics.java
@@ -0,0 +1,16 @@
+package org.usfirst.frc.team126.robot.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class Pneumatics extends Subsystem {
+
+	public void initDefaultCommand() {
+	}
+
+	public static void extend() {
+		//TODO do some neat stuff here
+	}
+	public static void retract() {
+		//TODO do some neat stuff here
+	}
+}
