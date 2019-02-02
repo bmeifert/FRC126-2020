a56e71fb5ed0aac15d0632dcddc28b6115fc0cb5
Keith <kwmeifert@gmail.com>
2019-02-02 14:54:27 -0500

    Updated lidar prescision drive
    it's neat

--- frc126-2019/src/main/java/org/usfirst/frc/team126/robot/commands/DriveWithJoysticks.java
+++ frc126-2019/src/main/java/org/usfirst/frc/team126/robot/commands/DriveWithJoysticks.java
@@ -5,7 +5,7 @@ import org.usfirst.frc.team126.robot.RobotMap;
 import edu.wpi.first.wpilibj.command.Command;
 
 public class DriveWithJoysticks extends Command {
-	double fb, lr, rot, tl, tr, x, y, v;
+	double fb, lr, rot, tl, tr, x, y, v, maxSpeed;
 	int smoothFactor = 5;
 	boolean xboxLTrig, xboxRTrig, xboxA, xboxB, xboxX, xboxY, xboxLStick, xboxRStick;
 	boolean isCurved = true;
@@ -88,7 +88,27 @@ public class DriveWithJoysticks extends Command {
 
 		}
 		else {
-			Robot.driveBase.Drive(fb, rot, isCurved, isSmoothed, smoothFactor); // Drive with set values
+			double lidarDistance = Robot.distance.getDistance();
+			if(lidarDistance < 95 && lidarDistance != 0) {
+				maxSpeed = 1 + ((lidarDistance - 95) / 40);
+				if(maxSpeed < 0.5) {
+					maxSpeed = 0.5;
+				}
+				fb *= maxSpeed;
+				/*
+				if(lidarDistance < 75) {
+					fb = -0.3 + (lidarDistance - 75) / 20;
+					if(fb < -0.5) {
+						fb = -0.5;
+					}
+				}
+				*/
+				Robot.driveBase.Drive(fb, rot, isCurved, isSmoothed, smoothFactor);
+			}
+			else {
+				Robot.driveBase.Drive(fb, rot, isCurved, isSmoothed, smoothFactor); // Drive with set values
+			}
+
 			Robot.intake.setIntake(lr); // Set intake to triggers
 			//System.out.println("Driving");
 		}
