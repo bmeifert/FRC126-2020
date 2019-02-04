453a58ea16d85191982f4bad04137201b32d49f9
Keith <kwmeifert@gmail.com>
2019-02-04 18:10:53 -0500

    Keeping different states clean

--- frc126-2019/src/main/java/org/usfirst/frc/team126/robot/subsystems/Lift.java
+++ frc126-2019/src/main/java/org/usfirst/frc/team126/robot/subsystems/Lift.java
@@ -1,27 +1,40 @@
 package org.usfirst.frc.team126.robot.subsystems;
 
+import org.usfirst.frc.team126.robot.Robot;
+
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 public class Lift extends Subsystem {
 	public static enum liftPos { 
-		first, second, third, zero, operator
+		first, second, third, zero, free
 	}
 	public static enum liftStates {
-		nothomed, ready, moving, bottomlimit, toplimit, estop
+		notzeroed, ready, moving, estop
+	}
+	public static enum limitStates {
+		bottomLimit, topLimit, ok
 	}
 	public static liftPos targetPos = null;
 	public static liftPos currentPos = null;
 	public static liftStates lState = null;
-	public double encoderVal = 0;
+	public static limitStates limitState = null;
+	public static double encoderVal = 0;
 
 	public void initDefaultCommand() {
+	}
+	public static void resetLift() {
+		lState = liftStates.notzeroed;
+		targetPos = liftPos.zero;
+		currentPos = liftPos.free;
+		limitState = limitStates.ok;
+
 	}
 	public static void setTargetPos(liftPos lPos) {
 		if(lState == liftStates.ready) {
 			targetPos = lPos;
 			lState = liftStates.moving;
 		}
-		else if(lState == liftStates.estop || lState == liftStates.nothomed){ // if the lift is having a major issue (not zeroed or emergency stopped)
+		else if(lState == liftStates.estop || lState == liftStates.notzeroed){ // if the lift is having a major issue (not zeroed or emergency stopped)
 			System.out.println("LIFT MOVE FAILED -- LIFT CRITICAL ERROR");
 		}
 		else { // if the lift is in any other state, e.g. already moving to another position
@@ -30,6 +43,14 @@ public class Lift extends Subsystem {
 
 	}
 	public static void moveLift(double newPos) {
-		//TODO do some neat stuff here
+		if(Robot.limitSwitch.get() == false) {
+			limitState = limitStates.bottomLimit;
+		}
+		else if(Robot.limitSwitch2.get() == false) {
+			limitState = limitStates.topLimit;
+		}
+		else {
+			limitState = limitStates.ok;
+		}
 	}
 }
