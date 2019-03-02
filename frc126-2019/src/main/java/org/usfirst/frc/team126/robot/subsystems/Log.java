c1c46eb681237fad6bfae6e6384b155009f3c7aa
Keith <kwmeifert@gmail.com>
2019-03-02 11:26:26 -0500

    Made log better

--- frc126-2019/src/main/java/org/usfirst/frc/team126/robot/subsystems/Log.java
+++ frc126-2019/src/main/java/org/usfirst/frc/team126/robot/subsystems/Log.java
@@ -1,16 +1,19 @@
 package org.usfirst.frc.team126.robot.subsystems;
 
+import java.util.Arrays;
+
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 public class Log extends Subsystem {
-
+	static String[] mutedSystems = new String[]{}; // MUTED SYSTEMS
 	public void initDefaultCommand() {
 	}
-
-	public static void print(int category, String header, String content) { // category: 0-2 INFO - WARN - CRIT
+	public static void print(int category, String header, String content) {
 		/*
-		* INFO - General logging, should not be spammed, eg. [INFO]
-		*
+		* (0) INFO - General logging, should not be spammed, eg. [INFO]
+		* (1) WARN - Messages that should be looked at after a match (eg. Gyro inaccuracies, low voltage, etc.)
+		* (2) CRIT - For problems that could affect robot operation (eg. high amperage, sensor failures, etc.)
+		* (3) EMER - For problems that could damage the robot (eg. mechanical limit overruns, range extremes, etc.)
 		*/
 		String catheader;
 		if(category == 0) {
@@ -23,8 +26,13 @@ public class Log extends Subsystem {
 			catheader = ">>[EMER]";
 			System.out.println(">>=================");
 		}
-		System.out.println(catheader + header + ": " + content);
-		if(category == 3) {
+		if(Arrays.asList(mutedSystems).contains(header) && category < 3) {
+
+		} else {
+			System.out.println(catheader + header + ": " + content);
+		}
+
+		if(category > 2) {
 			System.out.println(">>=================");
 		}
 	}
