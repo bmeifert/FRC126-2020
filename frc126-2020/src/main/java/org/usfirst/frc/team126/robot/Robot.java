16d64cb29ec8c480d8d7008ccc583ba1bfd05976
bmeifert <32942240+bmeifert@users.noreply.github.com>
2020-02-17 12:30:00 -0500

    Update Robot.java

--- frc126-2020/src/main/java/org/usfirst/frc/team126/robot/Robot.java
+++ frc126-2020/src/main/java/org/usfirst/frc/team126/robot/Robot.java
@@ -36,6 +36,7 @@ public class Robot extends TimedRobot {
 	public static CANSparkMax spark1 = new CANSparkMax(10, CANSparkMaxLowLevel.MotorType.kBrushless);
 	public static CANSparkMax spark2 = new CANSparkMax(11, CANSparkMaxLowLevel.MotorType.kBrushless);
 	public static TalonFX falcon1 = new TalonFX(12);
+	public static VictorSPX victor1 = new VictorSPX(50);
 
 	public double robotID;
 	double currentFalconSpeed;
